e52fb3766feb875d4cc429d555eccfa0ab6372a7
NJ <47932239+jaroussie@users.noreply.github.com>
2026-04-18 19:38:20 +0200

    Update images

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,9 +1,9 @@
 
-import __ASTRO_IMAGE_IMPORT_Z9VhD5 from "/src/images/1.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md";
-import __ASTRO_IMAGE_IMPORT_ZRQ8dR from "/src/images/2.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md";
-import __ASTRO_IMAGE_IMPORT_Z1AKXNE from "/src/images/3.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md";
-import __ASTRO_IMAGE_IMPORT_Z2jFOor from "/src/images/4.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md";
-import __ASTRO_IMAGE_IMPORT_22AsOH from "/src/images/5.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md";
-import __ASTRO_IMAGE_IMPORT_1jFCeU from "/src/images/6.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md";
-export default new Map([["/src/images/1.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z9VhD5], ["/src/images/2.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md", __ASTRO_IMAGE_IMPORT_ZRQ8dR], ["/src/images/3.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md", __ASTRO_IMAGE_IMPORT_Z1AKXNE], ["/src/images/4.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md", __ASTRO_IMAGE_IMPORT_Z2jFOor], ["/src/images/5.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md", __ASTRO_IMAGE_IMPORT_22AsOH], ["/src/images/6.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md", __ASTRO_IMAGE_IMPORT_1jFCeU]]);
+import __ASTRO_IMAGE_IMPORT_Z1DRnuy from "/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md";
+import __ASTRO_IMAGE_IMPORT_rMpCM from "/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md";
+import __ASTRO_IMAGE_IMPORT_Z2vIU2N from "/src/images/3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md";
+import __ASTRO_IMAGE_IMPORT_Zp46Ts from "/src/images/4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md";
+import __ASTRO_IMAGE_IMPORT_1GAGdS from "/src/images/5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md";
+import __ASTRO_IMAGE_IMPORT_Z1gUDrH from "/src/images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md";
+export default new Map([["/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z1DRnuy], ["/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md", __ASTRO_IMAGE_IMPORT_rMpCM], ["/src/images/3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md", __ASTRO_IMAGE_IMPORT_Z2vIU2N], ["/src/images/4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md", __ASTRO_IMAGE_IMPORT_Zp46Ts], ["/src/images/5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md", __ASTRO_IMAGE_IMPORT_1GAGdS], ["/src/images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md", __ASTRO_IMAGE_IMPORT_Z1gUDrH]]);
 		
