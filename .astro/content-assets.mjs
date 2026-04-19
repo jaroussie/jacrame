a959a996603b13811b137260a95be4a64a7f7296
NJ <47932239+jaroussie@users.noreply.github.com>
2026-04-19 18:55:33 +0200

    delete redundant post

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,9 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_Z1DRnuy from "/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md";
 import __ASTRO_IMAGE_IMPORT_rMpCM from "/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md";
-import __ASTRO_IMAGE_IMPORT_Z2vIU2N from "/src/images/3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md";
-import __ASTRO_IMAGE_IMPORT_Zp46Ts from "/src/images/4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md";
-import __ASTRO_IMAGE_IMPORT_1GAGdS from "/src/images/5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md";
-import __ASTRO_IMAGE_IMPORT_Z1gUDrH from "/src/images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md";
-export default new Map([["/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z1DRnuy], ["/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md", __ASTRO_IMAGE_IMPORT_rMpCM], ["/src/images/3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F3.md", __ASTRO_IMAGE_IMPORT_Z2vIU2N], ["/src/images/4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F4.md", __ASTRO_IMAGE_IMPORT_Zp46Ts], ["/src/images/5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F5.md", __ASTRO_IMAGE_IMPORT_1GAGdS], ["/src/images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F6.md", __ASTRO_IMAGE_IMPORT_Z1gUDrH]]);
+export default new Map([["/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z1DRnuy], ["/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md", __ASTRO_IMAGE_IMPORT_rMpCM]]);
 		
