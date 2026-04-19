165fee0a2bcd9c98e102df3b35b14e369cd9d03d
NJ <47932239+jaroussie@users.noreply.github.com>
2026-04-19 18:56:42 +0200

    Update content-assets.mjs

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
 import __ASTRO_IMAGE_IMPORT_Z1DRnuy from "/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md";
-import __ASTRO_IMAGE_IMPORT_rMpCM from "/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md";
-export default new Map([["/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z1DRnuy], ["/src/images/2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2.md", __ASTRO_IMAGE_IMPORT_rMpCM]]);
+export default new Map([["/src/images/1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_Z1DRnuy]]);
 		
